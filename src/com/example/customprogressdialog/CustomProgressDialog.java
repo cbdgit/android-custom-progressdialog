61c417154acf84c929ab491b28e42b2a31e663b5
Shams <shams.mobi@gmail.com>
2015-03-27 15:36:39 +0530

    Update CustomProgressDialog.java

--- src/com/example/customprogressdialog/CustomProgressDialog.java
+++ src/com/example/customprogressdialog/CustomProgressDialog.java
@@ -11,9 +11,9 @@ import android.widget.TextView;
 
 public class CustomProgressDialog extends ProgressDialog{
 	
-	private Animation animation;
+//	private Animation animation;
 	private Context context;
-	private ImageView imageView;
+//	private ImageView imageView;
 	private TextView textView;
 	private String msg;
 	
@@ -47,14 +47,14 @@ public class CustomProgressDialog extends ProgressDialog{
 	public void show() {
 		super.show();
 		
-		ImageView imageView = (ImageView) findViewById(R.id.animation);
+	/*	ImageView imageView = (ImageView) findViewById(R.id.animation);
 	    imageView.setBackgroundResource(R.drawable.spinnerwhite);
 	    
 	    textView = (TextView) findViewById(R.id.message);
 	    textView.setText(msg);
 	    
 	    animation = AnimationUtils.loadAnimation(super.getContext(), R.anim.rotate);
-	    imageView.startAnimation(animation);
+	    imageView.startAnimation(animation);*/
 	}
 	
 	@Override
